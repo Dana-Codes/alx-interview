278a3e94545fac264c084be9d6389570758f339d
CodingbyDaniel <109753547+Dana-Codes@users.noreply.github.com>
2024-05-30 23:43:27 +0100

    done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,125 +1,69 @@
+
 #!/usr/bin/python3
 """
-N queens
+Solution to the nqueens problem
 """
 import sys
 
 
-def diagonals(results, N):
+def backtrack(r, n, cols, pos, neg, board):
     """
-    list with diagonals
+    backtrack function to find solution
     """
-    # fill diagonals
-    diagonals = []
-    for i in results:
-        # up-left
-        it_row = i[0]
-        it_col = i[1]
-        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
-            if [it_row, it_col] not in diagonals:
-                diagonals.append([it_row, it_col])
-            it_row -= 1
-            it_col -= 1
+    if r == n:
+        res = []
+        for l in range(len(board)):
+            for k in range(len(board[l])):
+                if board[l][k] == 1:
+                    res.append([l, k])
+        print(res)
+        return
 
-        # up-right
-        it_row = i[0]
-        it_col = i[1]
-        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
-            if [it_row, it_col] not in diagonals:
-                diagonals.append([it_row, it_col])
-            it_row -= 1
-            it_col += 1
-
-        # up-right
-        it_row = i[0]
-        it_col = i[1]
-        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
-            if [it_row, it_col] not in diagonals:
-                diagonals.append([it_row, it_col])
-            it_row += 1
-            it_col -= 1
-
-        # down-right
-        it_row = i[0]
-        it_col = i[1]
-        while it_col >= 0 and it_col < N and it_row >= 0 and it_row < N:
-            if [it_row, it_col] not in diagonals:
-                diagonals.append([it_row, it_col])
-            it_row += 1
-            it_col += 1
-
-    return diagonals
-
-
-def isSafe(row, col, results, N):
-    """
-    know if safe a position
-    """
-    # validate columns
-    for _row in range(N):
-        if [_row, col] in results:
-            return False
-
-    return not [row, col] in diagonals(results, N)
-
-
-def chess(N):
-    """
-    iterate the positions
-    """
-    result = []
-    row = 0
-    col = 0
-
-    while row < N:
-        while col < N:
-            if isSafe(row, col, result, N):
-                result.append([row, col])
-                break
-            col += 1
-
-        # if a new position not exists in results
-        if len(result) != (row + 1):
-            row -= 1
-            if row < 0:
-                break
-            col = result[row][1] + 1
-            del result[row]
+    for c in range(n):
+        if c in cols or (r + c) in pos or (r - c) in neg:
             continue
-        elif len(result) == N:
-            print(result)
-            col += 1
-            del result[row]
-            continue
-        row += 1
-        col = 0
+
+        cols.add(c)
+        pos.add(r + c)
+        neg.add(r - c)
+        board[r][c] = 1
+
+        backtrack(r+1, n, cols, pos, neg, board)
+
+        cols.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board[r][c] = 0
 
 
-def start():
+def nqueens(n):
     """
-    N queens
+    Solution to nqueens problem
+    Args:
+        n (int): number of queens. Must be >= 4
+    Return:
+        List of lists representing coordinates of each
+        queen for all possible solutions
     """
-    args = sys.argv
+    cols = set()
+    pos_diag = set()
+    neg_diag = set()
+    board = [[0] * n for i in range(n)]
 
-    # Usage error
-    if len(args) is not 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+    backtrack(0, n, cols, pos_diag, neg_diag, board)
 
-    # type value error
-    try:
-        int(args[1])
-    except:
-        print("N must be a number")
-        sys.exit(1)
-
-    # less than 4 error
-    if int(args[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    N = int(args[1])
-    chess(N)
 
 if __name__ == "__main__":
-    start()
+    n = sys.argv
+    if len(n) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        nn = int(n[1])
+        if nn < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(nn)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
